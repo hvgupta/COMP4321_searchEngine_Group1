9fe62f1d80391f3371be33c01933443f785cac43
smokingpuddle58 <zzhanggk@connect.ust.hk>
2024-03-22 22:23:51 +0800

    Updated crawler.py

--- src/crawler.py
+++ src/crawler.py
@@ -3,14 +3,12 @@ import urllib.request
 from urllib.parse import urljoin, urlparse
 import asyncio
 from zlib import crc32
-import pydoc
 
 from bs4 import BeautifulSoup as bsoup
 import requests
 import sqlite3
 import datetime
 import dateutil.parser
-import json
 
 import indexer
 
@@ -31,7 +29,7 @@ def get_soup(url: str) -> bsoup:
 
     # Check if the request is successful or not.
     # If not then we assume the website is invalid, or the server is not responding.
-    if statCode is not -1:
+    if statCode != -1:
         if statCode not in range(200, 399):
             print("Error code " + str(statCode) + ". Please check your input and try again.")
             exit(255)
@@ -82,21 +80,67 @@ def get_info(cur_url, soup, parent_url=None):
     cur_url_parsed = urlparse(cur_url)
     cur_url_parsed = (cur_url_parsed.scheme + "://" + cur_url_parsed.netloc + cur_url_parsed.path)
 
-    page_id = crc32(cur_url_parsed)
+    if parent_url is not None:
+        par_url_parsed = urlparse(parent_url)
+        par_url_parsed = (par_url_parsed.scheme + "://" + par_url_parsed.netloc + par_url_parsed.path)
+    else:
+        par_url_parsed = None
 
-    last_modif = soup.find("meta", attrs={"http-equiv": "last-modified"})
+    cur_page_id = crc32(str.encode(cur_url_parsed))
+    parent_page_id = crc32(str.encode(par_url_parsed)) if parent_url is not None else None
+
+    last_modif = soup.find("meta", attrs={"class": "lastModified"})
     if last_modif is None:  # Find the date of the website
-        last_modif = soup.find("meta", attrs={"name": "date"})
+        last_modif = soup.find("meta", attrs={"name": "lastModified"})
+
 
     size = soup.find("meta", attrs={"name": "size"})
     if size is None:
         size = len(soup.find("body").text)
 
-
-def insert_data_into_database(data: list):
-    pass
+    return cur_page_id, parent_page_id, title, cur_url_parsed, last_modif, size
 
 
 def recursively_crawl(num_pages: int, url: str):
-    pass
-# Proposed: Using CRC32 to convert word into integer, and then use it as the primary key.
+    count = 0
+    queue = []  # Queue for BFS
+
+    visited = []  # Visited pages
+
+    # Put the link inside the queue
+    queue.append(url)
+
+    while num_pages > 0:
+        count += 1
+        cur_url = queue.pop(0)
+        visited.append(cur_url)
+
+        soup = get_soup(cur_url)
+
+        all_url = get_sub_link(cur_url, soup)
+
+        for url in all_url:
+            # Ensuring that cycles does not exist.
+            if url not in visited and url not in queue:
+                queue.append(url)
+
+
+        print(str(count),end=" ")
+        print(get_info(cur_url, soup))
+
+        num_pages -= 1
+    # Proposed: Using CRC32 to convert word into integer, and then use it as the primary key.
+
+
+def main():
+    # - Number of pages that will be crawled / indexed -
+    MAX_NUM_PAGES = 8
+
+    # - The URL that the program will be crawled -
+    URL = "https://comp4321-hkust.github.io/testpages/testpage.htm"
+
+    recursively_crawl(MAX_NUM_PAGES, URL)
+
+
+if __name__ == '__main__':
+    main()
--- src/indexer.py
+++ src/indexer.py
@@ -12,15 +12,16 @@ with open('files/stopwords.txt', 'r') as file:
     stopwords = file.read().split()
 
 
+def removeStopWords(words: list) -> list:
+    return [word for word in words if word not in stopwords]
+
+
 def tokenize(soup: bsoup) -> list:
     if soup is None:
         return []
     return removeStopWords(regex.sub('', soup.find("body").text).split())
 
 
-def removeStopWords(words: list) -> list:
-    return [word for word in words if word not in stopwords]
-
-
 def stemWords(words: list) -> list:
     return [ps.stem(word) for word in words]
+
