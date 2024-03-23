2a8f373ca14f1dbfd9064fd8998ba235c264c458
hvgupta <hvgupta@outlook.in>
2024-03-23 15:03:53 +0800

    not completed yet

--- src/indexer.py
+++ src/indexer.py
@@ -2,25 +2,33 @@ from nltk.stem import PorterStemmer as Stemmer
 import re
 from bs4 import BeautifulSoup as bsoup
 import crawler
+import sqlite3
+import numpy as np
 
 # Remove all punctuations that are not letters.
 regex = re.compile('[^a-zA-Z]')
 
 ps = Stemmer()
 
+db = sqlite3.connect('files/wordCount.db')
+
 with open('files/stopwords.txt', 'r') as file:
     stopwords = file.read().split()
-
-
-def tokenize(soup: bsoup) -> list:
-    if soup is None:
-        return []
-    return removeStopWords(regex.sub('', soup.find("body").text).split())
-
-
-def removeStopWords(words: list) -> list:
-    return [word for word in words if word not in stopwords]
-
+    
 
 def stemWords(words: list) -> list:
     return [ps.stem(word) for word in words]
+
+def removeStopWords(words: list[str]) -> np.ndarray[str]:
+    wrdList: np.ndarray[str] = np.array(words)
+    stpwords: np.ndarray[str] = np.array(stopwords)
+    return wrdList[~np.isin(wrdList, stpwords)]
+
+def tokenize(soup: bsoup) -> list[str]:
+    if soup is None:
+        return []
+    return removeStopWords(regex.sub(' ', soup.find("body").text).split())
+
+def inverseIndexCreator(document:bsoup) -> None:
+    stemmedDocument:list[str] = stemWords(tokenize(bsoup))
+    
