1b5e7ef3c381037b944ceb1d63a9032011875f93
fjaklsdf <143928825+fjaklsdf@users.noreply.github.com>
2024-05-04 02:31:40 +0800

    figure out flask form submission

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
@@ -6,6 +7,12 @@ app = Flask(__name__)
 def searchbar():
     return render_template("index.html")
 
+@app.route("/search/", methods=['POST'])
+def submit_search():
+    query = request.form['searchbar']
+    print(query)
+
+    return render_template("search_results.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
